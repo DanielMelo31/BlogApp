7055166985f4f001ac1e8170d60e463c10a25d06
Daniel <danielmelo276@gmail.com>
2021-03-05 19:36:18 -0500

    initialcommit

---
+++ app/src/main/java/com/example/blogapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.blogapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.blogapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
